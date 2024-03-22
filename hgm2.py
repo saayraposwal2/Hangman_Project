beef5dcc85ec6c9bb3fc751e229e647a83423ba4
saayraposwal2 <154619552+saayraposwal2@users.noreply.github.com>
2024-03-22 09:02:01 +0530

    Add files via upload

--- hgm2.py
+++ hgm2.py
@@ -1,5 +1,5 @@
 import random
-
+#number of turns for one game play
 turns = 7
 
 print("Choose from the following topics: ")
@@ -8,6 +8,8 @@ n=int(input('''1.Fruits
 3.Flowers
 4.Vehicles
 '''))
+
+#vareity of topics
 
 fruits ='''Apple Banana Apricot Atemoya Avocados Blueberry Blackcurrant Ackee
 Cranberry Cantaloupe Cherry Dragonfruit	Dates Cherimoya	Fig Coconut Cape gooseberry
@@ -55,36 +57,25 @@ while turns > 0:
 
         else:
             print ("_",end=""),     
-            # and increase the failed counter with one
-            failed += 1    
+            failed += 1
 
-    # if failed is equal to zero
+#basic game mechanics
 
-    # print You Won
     if failed == 0:        
         print ("You won")
-    # exit the script
-        break            
-    # ask the user go guess a character
+        break
+    
+    #asking user for character
     guess = input("guess a character:") 
 
-    # set the players guess to guesses
     guesses += guess                    
 
-    # if the guess is not found in the secret word
-    if guess not in word:  
- 
-     # turns counter decreases with 1 (now 9)
-        turns -= 1        
- 
-    # print wrong
-        print ("Wrong")  
- 
-    # how many turns are left
+    # for when guess is not found in the word
+    if guess not in word:
+        turns -= 1
+        print ("Wrong")
         print ("You have", + turns, 'more guesses' )
  
-    # if the turns are equal to zero
-        if turns == 0:           
-    
-        # print "You Lose"
-            print ("You Lose"  )
+    #Game over when turns are 0
+        if turns == 0:
+            print ("GAME OVER!!!")
