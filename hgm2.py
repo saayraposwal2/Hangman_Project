cffca713afab18b7d570585aaec60043ec453648
saayraposwal2 <154619552+saayraposwal2@users.noreply.github.com>
2024-04-12 08:07:40 +0530

    Update hgm2.py

--- hgm2.py
+++ hgm2.py
@@ -1,75 +1,79 @@
 import random
-#number of turns for one game play
-turns = 7
+c="y"
+while c in "yY":
+    #number of turns for one game play
+    turns = 7
 
-print("Choose desired integer from the following topics: ")
-n=int(input('''1.Fruits
+    print("Choose from the following topics: ")
+    n=int(input('''1.Fruits
 2.Animals
 3.Flowers
 4.Vehicles
 '''))
 
-#vareity of topics
+    #vareity of topics
 
-fruits ='''apple banana apricot atemoya avocados blueberry blackcurrant ackee cranberry cantaloupe cherry dragonfruit dates cherimoya fig coconut cape gooseberry grapefruit sweetsop chempedak hazelnut honeyberries durian'''.split()
+    fruits ='''apple banana apricot atemoya avocados blueberry blackcurrant ackee cranberry cantaloupe cherry dragonfruit dates cherimoya fig coconut cape gooseberry grapefruit sweetsop chempedak hazelnut honeyberries durian'''.split()
 
-animals='''dog kitten cat fish hamster turtle goldfish rabbit mouse puppy parrot cow sheep chicken horse shrimp pig bee crab deer turkey ducks goat rabbit fish dove'''.split()
+    animals='''dog kitten cat fish hamster turtle goldfish rabbit mouse puppy parrot cow sheep chicken horse shrimp pig bee crab deer turkey ducks goat rabbit fish dove'''.split()
 
-flowers='''rose chrysanthemum daisy jasmine gerbera carnation poppy tulip lily lotus hibiscus peony sunflower lilac aster dandelion marigold dahlia lamium datura cosmos broom columbine'''.split()
+    flowers='''rose chrysanthemum daisy jasmine gerbera carnation poppy tulip lily lotus hibiscus peony sunflower lilac aster dandelion marigold dahlia lamium datura cosmos broom columbine'''.split()
 
-vehicles='''auto rickshaw ambulance aeroplane boat bicycle bus bike car crane cycle carriage cargo ship ferry gondolas helicopter houseboat jetpack limousine lorry motorcycle metro rowboat ropeway ship scooty tractor train'''.split()
+    vehicles='''auto rickshaw ambulance aeroplane boat bicycle bus bike car crane cycle carriage cargo ship ferry gondolas helicopter houseboat jetpack limousine lorry motorcycle metro rowboat ropeway ship scooty tractor train'''.split()
 
-if(n==1):
-    t=random.randint(0,len(fruits)-1)
-    word = fruits[t]
-elif(n==2):
-    t=random.randint(0,len(animals)-1)
-    word = animals[t]
-elif(n==3):
-    t=random.randint(0,len(flowers)-1)
-    word = flowers[t]
-elif(n==4):
-    t=random.randint(0,len(vehicles)-1)
-    word = vehicles[t]
+    if(n==1):
+        t=random.randint(0,len(fruits)-1)
+        word = fruits[t]
+    elif(n==2):
+        t=random.randint(0,len(animals)-1)
+        word = animals[t]
+    elif(n==3):
+        t=random.randint(0,len(flowers)-1)
+        word = flowers[t]
+    elif(n==4):
+        t=random.randint(0,len(vehicles)-1)
+        word = vehicles[t]
     
-else:
-    print("invalid input")
+    else:
+        print("invalid input")
     
-guesses = ''
+    guesses = ''
 
 
 
-while turns > 0:         
-    failed = 0             
+    while turns > 0:         
+        failed = 0             
 
        
-    for char in word:
-        if char in guesses:
-            print (char,end=""),    
+        for char in word:
+            if char in guesses:
+                print (char,end=""),    
 
-        else:
-            print ("_ ",end=""),     
-            failed += 1
+            else:
+                print ("_",end=""),     
+                failed += 1
 
-#basic game mechanics
+    #basic game mechanics
 
-    if failed == 0:
-        print("\nYES THAT WAS THE WORD!!!")
-        print ("\nYOU WON!!!!")
-        break
+        if failed == 0:        
+            print ("You won")
+            break
     
-    #asking user for character
-    guess = input("guess a character:") 
+        #asking user for character
+        guess = input("  guess a character:") 
 
-    guesses += guess                    
+        guesses += guess                    
 
-    # for when guess is not found in the word
-    if guess not in word:
-        turns -= 1
-        print ("Wrong")
-        print ("You have", + turns, 'more guesses' )
+        # for when guess is not found in the word
+        if guess not in word:
+            turns -= 1
+            print ("Wrong")
+            print ("You have",turns, 'more guesses' )
  
-    #Game over when turns are 0
-        if turns == 0:
-            print("The word was:",word)
-            print ("GAME OVER!!!")
+        #Game over when turns are 0
+            if turns == 0:
+                print("The word was:",word)
+                print ("GAME OVER!!!")
+                
+    c=input("Play Again? Y/N")
+print("Thank You For Playing!!!")
